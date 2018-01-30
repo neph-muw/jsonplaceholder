b0b56e1213a08fedc2c49ff576d6a69aeffd685c
Roman Mykitchak <deviphone@i.ua>
2018-01-30 18:44:52 +0100

    Initial Commit

---
+++ jsonplaceholder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jsonplaceholder
+//
+//  Created by Roman Mykitchak on 1/30/18.
+//  Copyright © 2018 Roman Mykitchak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
